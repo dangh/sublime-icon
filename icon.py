e23c3956f9a803c1cbbdbbec1e39974e90dbb051
Dang Huynh <huynhminhdang@gmail.com>
2019-04-06 05:38:44 +0800

    execute fileicon without chmod +x

--- icon.py
+++ icon.py
@@ -6,6 +6,6 @@ def plugin_loaded():
     fileicon = os.path.abspath(__file__ + '/../bin/fileicon')
     app_path = '/Applications/Sublime Text.app'
     icon_path = os.path.abspath(__file__ + '/../sublime-icon.icns')
-    no_icon = subprocess.call([fileicon, 'test', app_path])
+    no_icon = subprocess.call(['/usr/bin/env', 'bash', fileicon, 'test', app_path])
     if no_icon:
-        subprocess.call([fileicon, 'set', app_path, icon_path])
+        subprocess.call(['/usr/bin/env', 'bash', fileicon, 'set', app_path, icon_path])
