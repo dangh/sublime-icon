9666cd750f6be9a5fe70fa9444be10dfb8ec58db
Dang H <huynhminhdang@gmail.com>
2019-04-06 05:20:11 +0800

    Initial commit

---
+++ icon.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+
+
+def plugin_loaded():
+    fileicon = os.path.abspath(__file__ + '/../bin/fileicon')
+    app_path = '/Applications/Sublime Text.app'
+    icon_path = os.path.abspath(__file__ + '/../sublime-icon.icns')
+    no_icon = subprocess.call([fileicon, 'test', app_path])
+    if no_icon:
+        subprocess.call([fileicon, 'set', app_path, icon_path])
