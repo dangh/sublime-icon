02c4017771b27565eae7af252a83b4acd79e9963
Dang Huynh <huynhminhdang@gmail.com>
2019-12-01 15:33:19 +0800

    get application path dynamically

--- icon.py
+++ icon.py
@@ -4,7 +4,7 @@ import os
 
 def plugin_loaded():
     fileicon = os.path.abspath(__file__ + '/../bin/fileicon')
-    app_path = '/Applications/Sublime Text.app'
+    app_path = sublime.executable_path()[0:-28]
     icon_path = os.path.abspath(__file__ + '/../sublime-icon.icns')
     no_icon = subprocess.call(['/usr/bin/env', 'bash', fileicon, 'test', app_path])
     if no_icon:
